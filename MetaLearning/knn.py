416891534c84d683b327df2e8299c022956d2b7a
Nicolas Dugué <nicolas.dugue@loria.fr>
2016-04-07 23:57:24 +0200

    Add Feature Selection functions : fmax coded, tests on isko exemple

--- MetaLearning/knn.py
+++ MetaLearning/knn.py
@@ -2,7 +2,10 @@ from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 from sklearn.cross_validation import train_test_split
 
-matrix_iris=np.loadtxt("data/iris/iris.data")
+'''
+A simple program to build a knn classifier using IRIS data
+'''
+matrix_iris=np.loadtxt("../data/iris/iris.data")
 classes=matrix_iris[:,4]
 matrix_iris=matrix_iris[:,0:3]
 X_train, X_test, Y_train, Y_test = train_test_split(matrix_iris, classes, test_size=0.6)
---
+++ MetaLearning/pyfmax/fmax.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from scipy.sparse import csr_matrix, csc_matrix
+import numpy as np
+
+class MatrixClustered:
+	"""
+		This class allows to define a matrix which rows (objects) were clustered
+		Labels can be used to describe rows (objects) and columns (features)
+    	"""
+
+	def __init__(self, matrix, clustering, labels_row=[], labels_col=[]):
+		"""
+		Matrix and clustering should be at least passed.
+		Matrix should be a 2D Array or already a sparse (csr or csc) matrix.
+		Clustering should be an array where a value v at index i defines that the object i (row i in matrix) belongs to cluster v
+		Labels can be used to describe rows (objects) and columns (features) in the same way as the clustering object
+    		"""
+
+		self.matrix_csr = csr_matrix(matrix)
+
+		self.matrix_csc = csc_matrix(matrix)
+
+		self.clustering=clustering
+		
+		self.clusters=[]
+		for idx,elmt in enumerate(self.clustering):
+			elmt=int(elmt)
+			taille=(len(self.clusters) -1) 
+			if elmt >= taille:
+				for i in range(elmt - taille):
+					self.clusters.append([])
+			self.clusters[elmt].append(idx)
+
+		self.labels_row=labels_row
+
+		self.labels_col=labels_col
+
+		self.sum_rows=self.matrix_csr.sum(axis=1)
+
+		self.sum_cols=self.matrix_csc.sum(axis=0)
+		
+		self.ffmean=np.empty(self.matrix_csr.shape[1])
+		self.ffmean.fill(-1.0)
+		
+
+		
+
+	def sum_row(self, i):
+		"""
+		Get the sum of row i
+    		"""
+		return self.sum_rows[i]
+
+	def sum_col(self, j):
+		"""
+		Get the sum of column j
+		Used in Feature Precision (Predominance)
+    		"""
+		return self.sum_cols[:,j]
+	
+	def sum_col_of_cluster(self, j, k):
+		"""
+		Get the sum of column j
+		Used in Feature Precision (Predominance)
+    		"""
+		column=self.matrix_csc.getcol(j).toarray();
+		sum=0
+		for idx,elmt in enumerate(column):
+			if (self.clustering[idx] == k):
+				sum+=elmt
+		return sum	
+	
+	def sum_cluster(self, i):
+		"""
+		Get the sum of cluster i
+		Used in feature recall
+    	"""
+		cluster=self.clusters[i]
+		sum=0
+		for row in cluster:
+			sum+=self.sum_row(row)
+		return sum
+	
+	def fp(self, j, k):
+		numerator=self.sum_col_of_cluster(j, k)
+		denominator =self.sum_cluster(k)
+		if denominator == 0:
+			return 0
+		else:
+			return numerator / denominator
+		
+	def fr(self, j, k):
+		numerator=self.sum_col_of_cluster(j, k)
+		denominator =self.sum_col(j)
+		if denominator == 0:
+			return 0
+		else:
+			return numerator / denominator
+	
+	def ff(self, j, k):
+		fr=self.fr(j,k)
+		fp=self.fp(j,k)
+		if fr == 0 and fp == 0:
+			return 0
+		else:
+			return (2*fr*fp) /(fr + fp) 
+		
+	def ff_mean(self, j):
+		mean=0
+		for k in range(len(self.clusters)):
+			mean+=self.ff(j,k)
+		self.ffmean[j]=mean / len(self.clusters)
+		return self.ffmean[j]
+	
+	def ff_mean_all(self):
+		mean=0
+		for j in range(self.get_cols_number()):
+			if (self.ffmean[j] == -1):
+				self.ff_mean(j)
+			mean+=self.ffmean[j]
+		return mean / self.get_cols_number()
+			
+		
+	def get_row_label(self, i):
+		if len(self.labels_row) == len(self.clustering):
+			return self.labels_row[i]
+		else:
+			return i+""
+		
+	def get_col_label(self, j):
+		if len(self.labels_col) > 0:
+			return self.labels_col[j]
+		else:
+			return j+""
+	
+	def get_rows_number(self):
+		return self.matrix_csr.shape[0]
+	
+	def get_cols_number(self):
+		return self.matrix_csr.shape[1]
+
+	def __str__(self):
+		"""
+		toString()
+    		"""
+		return "Matrix CSR (ordered by rows) :\n" + str(self.matrix_csr)+ "\nMatrix CSC (ordered by columns): \n"+ str(self.matrix_csc) + "\nColumns labels (features) " + str(self.labels_col) + "\nRows labels (objects) " + str(self.labels_row) + "\nClustering :  " + str(self.clustering)+"\nClusters : "+str(self.clusters)
+		
