6454c20ec8c7421a77c086b903b2ad4066074193
Nicolas Dugué <dugue.nic@gmail.com>
2016-04-08 17:31:42 +0200

    First tests on IRIS data with knn

--- MetaLearning/knn.py
+++ MetaLearning/knn.py
@@ -8,13 +8,28 @@ A simple program to build a knn classifier using IRIS data
 matrix_iris=np.loadtxt("../data/iris/iris.data")
 classes=matrix_iris[:,4]
 matrix_iris=matrix_iris[:,0:3]
-X_train, X_test, Y_train, Y_test = train_test_split(matrix_iris, classes, test_size=0.6)
+X_train, X_test, Y_train, Y_test = train_test_split(matrix_iris, classes, test_size=0.5)
 
 neigh = KNeighborsClassifier(n_neighbors=10)
 neigh.fit(X_train, Y_train) 
 Y_returned=neigh.predict(X_test)
 results=(Y_returned == Y_test)
 print results
-print neigh.predict_proba(X_test)
 
+import pyfmax.fmax as fm
 
+fm_matrix=fm.MatrixClustered(X_train, Y_train)
+contrasted_matrix=fm_matrix.contrast_and_select_matrix()
+neigh.fit(contrasted_matrix, Y_train) 
+Y_returned=[]
+for vector in X_test:
+    best=0
+    max=-1
+    for k in range(3):
+        vector_contrasted=fm_matrix.contrast_and_select_features(vector, k)
+        if np.max(neigh.predict_proba(vector_contrasted)) > max:
+            max=np.max(neigh.predict_proba(vector_contrasted))
+            best=k
+    Y_returned.append(best)
+results=(Y_returned == Y_test)
+print results
